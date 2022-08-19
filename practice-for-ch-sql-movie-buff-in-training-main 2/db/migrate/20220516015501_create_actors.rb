4d393d2109e7da5e3753969a508a8e941216d7fd
Daniel Li <Dli53@outlook.com>
2022-08-19 14:06:56 -0400

    hw delete

--- practice-for-ch-sql-movie-buff-in-training-main 2/db/migrate/20220516015501_create_actors.rb
+++
@@ -1,9 +0,0 @@
-class CreateActors < ActiveRecord::Migration[7.0]
-  def change
-    create_table :actors do |t|
-      t.string :name, null: false, index: true
-
-      t.timestamps
-    end
-  end
-end
